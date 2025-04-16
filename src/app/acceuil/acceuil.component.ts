5ae174801e2a6e17d9779c5fefd9711a31005b06
suilahi <suilahizakaria@gmail.com>
2025-04-16 12:42:47 +0100

    navbar et acceuil

---
+++ src/app/acceuil/acceuil.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-acceuil',
+  imports: [],
+  templateUrl: './acceuil.component.html',
+  styleUrl: './acceuil.component.css'
+})
+export class AcceuilComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AcceuilComponent } from './acceuil/acceuil.component';
+import { QuizComponent } from './quiz/quiz.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavbarComponent,AcceuilComponent,QuizComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
